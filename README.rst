dc7f6255423b132a23db477d4b6fe153156ce7b8
gryf <gryf73@gmail.com>
2013-05-12 18:14:20 +0200

    Renamed repository. Added common lib for all extfs plugins. Added uadf and ulzx.

--- README.rst
+++ README.rst
@@ -1,16 +1,129 @@
-=================================
-ulha extfs for Midnight Commander
-=================================
+========================
+Midnight Commander extfs
+========================
 
-This is Midnight Commander extfs plugin for handling lha/lzh archives.
-It requires `lha <http://lha.sourceforge.jp>`_ free LHA implementation to work.
+Those are Midnight Commander extfs plugins for handling several archive types.
+
+Installation
+============
+See individual installation plugins below. Basically it come down to:
+
+* copying ``extfslib.py`` and plugin files to ``~/.local/share/mc/extfs/``
+* installing binary handlers (lha, unlzx, xdms and unadf)
+* adding an entry in ``~/.config/mc/mc.ext``::
+
+    # arch
+    regex/\.pattern$
+         Open=%cd %p/handler_filename://
+
+ULha
+====
+ULha is an extfs plugin which can be used with lha/lzh/lharc archives.
+Personally, I've use it almost exclusively for archives created long time ago
+on my Amiga. Both reading and writing into archive was implemented.
+
+Requirements
+------------
+ULha requires `free lha <http://lha.sourceforge.jp>`_ implementation to work.
 
 Installation
 ------------
-* copy ``ulha.py`` to ``~/.local/share/mc/extfs/ulha``
-* add or change entry for lha/lzh files handle in ``~/.config/mc/mc.ext``::
+* copy ``extfslib.py`` and ``ulha`` to ``~/.local/share/mc/extfs/``
+* add or change entry for files handle in ``~/.config/mc/mc.ext``::
 
     # lha
     regex/\.[lL]([Hh][aA]|[Zz][hH])$
          Open=%cd %p/ulha://
          View=%view{ascii} lha l %f
+
+ULzx
+====
+ULzx is an extfs plugin which can be used to browse and extract lzx archives,
+which are known almost exclusively from Amiga.
+
+Due to limitations of
+`unlzx <ftp://us.aminet.net/pub/aminet/misc/unix/unlzx.c.gz.readme>`_ tools,
+only reading is supported. Also be aware, that
+`unlzx <ftp://us.aminet.net/pub/aminet/misc/unix/unlzx.c.gz.readme>`_ tool
+cannot extract files individually, so copying entire archive content is not
+recommended, since on every file a full archive extract would be performed,
+which in the end would have impact on performance.
+
+Requirements
+------------
+ULzx requires
+`unlzx <ftp://us.aminet.net/pub/aminet/misc/unix/unlzx.c.gz.readme>`_ tool.
+
+Installation
+------------
+* copy ``extfslib.py`` and ``ulzx`` to ``~/.local/share/mc/extfs/``
+* add or change entry for files handle in ``~/.config/mc/mc.ext``::
+
+    # lzx
+    regex/\.[lL][zZ][xX]$
+         Open=%cd %p/ulzx://
+         View=%view{ascii} unlzx -v %f
+
+UAdf
+====
+UAdf is an extfs plugin suitable for reading .adf, .adz and .dms Amiga floppy
+disk images. Due to limitations of the
+`unadf <http://freecode.com/projects/unadf>`_, file access inside disk image is
+read only.
+
+Note, that in case of no-dos images, directory listing will be empty.
+
+Requirements
+------------
+
+It requires the `unadf <http://freecode.com/projects/unadf>`_ utility.
+Unfortunately, the page containing original sources doesn't exists
+anymore. Luckily, there is a copy of the source (and useful patches) in `Debian
+repository <http://packages.debian.org/sid/unadf>`_.
+
+There should be one change made to the source of unadf, though. While using
+"-lr" switch, unadf by default also displays comments next to file name,
+separated by the comma. If comment or filename contains comma sign, there is no
+way to distinguish where filename ends and comment starts. In ``extras``
+directory there is a patch for fixing this - no comments would be displayed by
+default.
+
+First, grab the sources and Debian patches. Apply them. To do this manually,
+extract ``unadf_0.7.11a-3.debian.tar.gz`` and ``unadf_0.7.11a.orig.tar.gz`` into
+some temporary directory. Apply patches in order like in debian/patches/series
+file. Then apply patch from extras directory::
+
+    $ mkdir temp
+    $ cd temp
+    $ tar zxf ~/Downloads/unadf_0.7.11a-3.debian.tar.gz
+    $ tar zxf ~/Downloads/unadf_0.7.11a.orig.tar.gz
+    $ cd unadf-0.7.11a
+    $ for i in `cat ../debian/patches/series`; do
+    > patch -Np1 < "../debian/patches/${i}"
+    > done
+    $ patch -Np1 < [path_to_this_repo]/extras/unadf_separate_comment.patch
+    $ make
+    $ cp Demo/unadf [destination_path]
+
+``unadf`` binary should be placed in ``$PATH``.
+
+For optional dms support, `xdms <http://zakalwe.fi/~shd/foss/xdms/>`_ utility is
+needed.
+
+Installation
+------------
+* copy ``extfslib.py`` and ``uadf`` to ``~/.local/share/mc/extfs/``
+* add or change entry for files handle in ``~/.config/mc/mc.ext``::
+
+    # adf
+    type/^Amiga\ .* disk
+        Open=%cd %p/uadf://
+        View=%view{ascii} unadf -lr %f
+
+    # adz
+    regex/\.([aA][dD][zZ])$
+        Open=%cd %p/uadf://
+
+    # dms
+    regex/\.([dD][mM][sS])$
+        Open=%cd %p/uadf://
---
+++ extfslib.py
@@ -0,0 +1,177 @@
+"""
+extfslib is a library which contains Archive class to support writing extfs
+plugins for Midnight Commander.
+
+Tested against python 2.7 and mc 4.8.7
+
+Changelog:
+    1.0 Initial release
+
+Author: Roman 'gryf' Dobosz <gryf73@gmail.com>
+Date: 2013-05-12
+Version: 1.0
+Licence: BSD
+"""
+import os
+import sys
+import re
+from subprocess import check_output, CalledProcessError
+
+
+class Archive(object):
+    """Archive handle. Provides interface to MC's extfs subsystem"""
+    LINE_PAT = re.compile("^(?P<size>)\s"
+                          "(?P<perms>)\s"
+                          "(?P<uid>)\s"
+                          "(?P<gid>)\s"
+                          "(?P<date>)\s+"
+                          "(?P<time>)\s"
+                          "(?P<fpath>)")
+    ARCHIVER = "archiver_name"
+    CMDS = {"list": "l",
+            "read": "r",
+            "write": "w",
+            "delete": "d"}
+
+    def __init__(self, fname):
+        """Prepare archive content for operations"""
+        if not os.path.exists(fname):
+            raise OSError("No such file or directory `%s'" % fname)
+        self._arch = fname
+        self._contents = self._get_dir()
+
+    def _map_name(self, name):
+        """MC still have a bug in extfs subsystem, in case of filepaths with
+        leading space. This is workaround to this bug, which replaces leading
+        space with tilda."""
+        if name.startswith(" "):
+            new_name = "".join(["~", name[1:]])
+            return new_name
+        return name
+
+    def _get_real_name(self, name):
+        """Get real filepath of the file. See _map_name docstring for
+        details."""
+        for item in self._contents:
+            if item['display_name'] == name:
+                return item['fpath']
+        return None
+
+    def _get_dir(self):
+        """Prepare archive file listing. Expected keys which every entry
+        should have are: size, perms, uid, gid, date, time, fpath and
+        display_name."""
+        contents = []
+
+        out = self._call_command("list")
+        if not out:
+            return
+
+        for line in out.split("\n"):
+            match = self.LINE_PAT.match(line)
+            if not match:
+                continue
+            entry = match.groupdict()
+            contents.append(entry)
+
+        return contents
+
+    def _call_command(self, cmd, src=None, dst=None):
+        """
+        Return status of the provided command, which can be one of:
+            write
+            read
+            delete
+            list
+        """
+        command = [self.ARCHIVER, self.CMDS.get(cmd), self._arch]
+
+        if src and dst:
+            command.append(src)
+            command.append(dst)
+        elif src or dst:
+            command.append(src and src or dst)
+
+        try:
+            output = check_output(command)
+        except CalledProcessError:
+            sys.exit(1)
+        return output
+
+    def list(self):
+        """Output contents of the archive to stdout"""
+        sys.stderr.write("Not supported")
+        return 1
+
+    def run(self, dst):
+        """Execute file out of archive"""
+        sys.stderr.write("Not supported")
+        return 1
+
+    def copyout(self, src, dst):
+        """Copy file out of archive"""
+        sys.stderr.write("Not supported")
+        return 1
+
+    def rm(self, dst):
+        """Remove file from archive"""
+        sys.stderr.write("Not supported")
+        return 1
+
+    def mkdir(self, dst):
+        """Create empty directory in archive"""
+        sys.stderr.write("Not supported")
+        return 1
+
+    def rmdir(self, dst):
+        """Removes directory from archive"""
+        sys.stderr.write("Not supported")
+        return 1
+
+    def copyin(self, dst, src=None):
+        """Copy file to the archive"""
+        sys.stderr.write("Not supported")
+        return 1
+
+
+def usage():
+    """Print out usage information"""
+    print ("Usage: %(prg)s {copyin,copyout} ARCHNAME SOURCE DESTINATION\n"
+           "or: %(prg)s list ARCHNAME\n"
+           "or: %(prg)s {mkdir,rm,rmdir,run} ARCHNAME TARGET" %
+           {"prg": sys.argv[0]})
+
+
+def parse_args(arch_class):
+    """Retrive and parse arguments from commandline and apply them into passed
+    arch_class class object."""
+    try:
+        if sys.argv[1] not in ('list', 'copyin', 'copyout', 'rm', 'mkdir',
+                               "run", "rmdir"):
+            usage()
+            sys.exit(2)
+    except IndexError:
+        usage()
+        sys.exit(2)
+
+    arch = src = dst = None
+    try:
+        arch = sys.argv[2]
+        if sys.argv[1] in ('copyin', 'copyout'):
+            src = sys.argv[3]
+            dst = sys.argv[4]
+        elif sys.argv[1] in ('rm', 'rmdir', 'run', 'mkdir'):
+            dst = sys.argv[3]
+    except IndexError:
+        usage()
+        sys.exit(2)
+
+    call_map = {'copyin': lambda a, s, d: arch_class(a).copyin(s, d),
+                'copyout': lambda a, s, d: arch_class(a).copyout(s, d),
+                'list': lambda a, s, d: arch_class(a).list(),
+                'mkdir': lambda a, s, d: arch_class(a).mkdir(d),
+                'rm': lambda a, s, d: arch_class(a).rm(d),
+                'rmdir': lambda a, s, d: arch_class(a).rmdir(d),
+                'run': lambda a, s, d: arch_class(a).run(d)}
+
+    return call_map[sys.argv[1]](arch, src, dst)
